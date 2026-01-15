5adcbe74c0c1de38caf557f357ec613265de3082
braiinybear <braiinybear@gmail.com>
2026-01-15 10:31:21 +0530

    cors policy modified

--- lib/cors.ts
+++ lib/cors.ts
@@ -4,10 +4,10 @@ import { NextResponse } from "next/server";
 export function withCors(response: NextResponse, request?: Request) {
   // Allowed origins
   const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
+    // "http://localhost:5173",
+    // "http://localhost:3000",
     "https://www.braiinybear.org",
-    "http://www.braiinybear.org",
+
   ];
 
   // Get the origin from the request
@@ -19,9 +19,6 @@ export function withCors(response: NextResponse, request?: Request) {
   } else if (!origin) {
     // For same-origin requests or if origin is missing
     response.headers.set("Access-Control-Allow-Origin", allowedOrigins[0]);
-    response.headers.set("Access-Control-Allow-Origin", allowedOrigins[1]);
-    response.headers.set("Access-Control-Allow-Origin", allowedOrigins[2]);
-    response.headers.set("Access-Control-Allow-Origin", allowedOrigins[3]);
   }
 
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
